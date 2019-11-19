8765361271b8b8dbe52f0f4c266a2721998f837e
Phusit Jitpun <51953483+phusitsiriyakorn@users.noreply.github.com>
2019-11-19 15:53:18 +0700

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,23 @@
+import time
+
+word = ["THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"]
+def SequencesV1():
+    total = 0
+    for i in range():
+        total += (i+1)
+    return(total)
+def asciiV1():
+    asciiv1 = {'A':65,'B':66,'C':67,'D':68,'E':69,'F':70,
+                'G':71,'H':72,'I':73,'J':74,'K':75,'L':76,
+                'M':77,'N':78,'O':79,'P':80,'Q':81,'R':82,
+                'S':83,'T':84,'U':85,'V':86,'W':87,'X':88,
+                'Y':89,'Z':90}
+    for i in word:
+        space = 0
+        asciiv11 = {}
+        asciiv11 = asciiv1+asciiv1
+        print(asciiv11) 
+asciiV1()
+start = time.time()
+print('answer: ', SequencesV1(n))
+print('time: ', (time.time()-start)*1000)
