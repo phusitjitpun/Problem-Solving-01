fcd02f2549d476ecdc5cec0c3946f1e855c0b075
Python <Python@Admins-iMac-6.local>
2020-01-07 16:05:44 +0700

    6

--- week6/EX_Hash.py
+++ week6/EX_Hash.py
@@ -9,24 +9,24 @@ def xhash():
   list_name8 = 'PHU'
   list_name9 = 'NAX'
   list_name10 = 'CHO'
-  hash1 = str(hash(list_name1)%10)
-  hash2 = str(hash(list_name2)%10)
-  hash3 = str(hash(list_name3)%10)
+  hash1 = hash(list_name1)
+  hash2 = hash(list_name2)
+  '''hash3 = str(hash(list_name3)%10)
   hash4 = str(hash(list_name4)%10)
   hash5 = str(hash(list_name5)%10)
   hash6 = str(hash(list_name6)%10)
   hash7 = str(hash(list_name7)%10)
   hash8 = str(hash(list_name8)%10)
   hash9 = str(hash(list_name9)%10)
-  hash10 = str(hash(list_name10)%10)
+  hash10 = str(hash(list_name10)%10)'''
   print(hash1)
   print(hash2)
-  print(hash3)
+  '''print(hash3)
   print(hash4)
   print(hash5)
   print(hash6)
   print(hash7)
   print(hash8)
   print(hash9)
-  print(hash10)
+  print(hash10)'''
 xhash()
