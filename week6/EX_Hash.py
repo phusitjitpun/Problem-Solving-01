6bddc23590efba965e2ba135326e6ec9eaf281ea
Python <Python@Admins-iMac-6.local>
2020-01-07 15:50:03 +0700

    6

---
+++ week6/EX_Hash.py
@@ -0,0 +1,32 @@
+def xhash():
+  list_name1 = 'TAN'
+  list_name2 = 'PRA'
+  list_name3 = 'MAC'
+  list_name4 = 'NAT'
+  list_name5 = 'ZAX'
+  list_name6 = 'FAS'
+  list_name7 = 'PEN'
+  list_name8 = 'PHU'
+  list_name9 = 'NAX'
+  list_name10 = 'CHO'
+  hash1 = str(hash(list_name1)%10)
+  hash2 = str(hash(list_name2)%10)
+  hash3 = str(hash(list_name3)%10)
+  hash4 = str(hash(list_name4)%10)
+  hash5 = str(hash(list_name5)%10)
+  hash6 = str(hash(list_name6)%10)
+  hash7 = str(hash(list_name7)%10)
+  hash8 = str(hash(list_name8)%10)
+  hash9 = str(hash(list_name9)%10)
+  hash10 = str(hash(list_name10)%10)
+  print(hash1)
+  print(hash2)
+  print(hash3)
+  print(hash4)
+  print(hash5)
+  print(hash6)
+  print(hash7)
+  print(hash8)
+  print(hash9)
+  print(hash10)
+xhash()
