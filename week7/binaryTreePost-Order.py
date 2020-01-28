a5bcdd688359796f85c2d2149bc368b958711210
Phusit Jitpun <51953483+phusitsiriyakorn@users.noreply.github.com>
2020-01-28 12:45:52 +0700

    7

---
+++ week7/binaryTreePost-Order.py
@@ -0,0 +1,7 @@
+def PostorderTraversal(self, root):
+    res = []
+    if root:
+        res = self.PostorderTraversal(root.left)
+        res = res + self.PostorderTraversal(root.right)
+        res.append(root.data)
+        return res
